b79e90ddd473b4c343cd74a51960f14c78822e2d
MilkMan90 <mlkaufman747@gmail.com>
2016-12-06 16:12:04 -0700

    fixed taylors comments

--- app/router.js
+++ app/router.js
@@ -7,9 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('reminders', {
-    path: '/'
-  });
+  this.route('reminders', {path: '/'});
 });
 
 export default Router;
