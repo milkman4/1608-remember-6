693642bdb41b7e8e4de499a950c305550af767cb
lrknaff <lacey.knaff@gmail.com>
2016-12-08 13:34:48 -0700

    render reminder from form to page

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  title: '',
+  date: '',
+  notes: '',
+
+  actions: {
+    addNew() {
+      const reminder = this.getProperties('title', 'date', 'notes');
+      this.get('store').createRecord('reminder', reminder).save().then(() => {
+        this.setProperties({ title: '', date: '', notes: '' });
+      });
+    }
+  }
 });
