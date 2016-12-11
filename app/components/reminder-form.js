c9594cc7c1b18fe6d222b5f864a99017ad079a73
MilkMan90 <mlkaufman747@gmail.com>
2016-12-11 16:29:40 -0700

    latest

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -2,18 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+  router: Ember.inject.service('-routing'),
 
   title: '',
   date: '',
   notes: '',
+  saveOrEdit: 'save',
 
   actions: {
-    addNew() {
-      let reminder = this.getProperties('title', 'date', 'notes');
-      reminder.date = new Date(reminder.date);
-      this.get('store').createRecord('reminder', reminder).save().then(() => {
-        this.setProperties({ title: '', date: '', notes: '' });
-      });
+    handleSave() {
+      let reminder = this.getProperties('title', 'date', 'notes', 'saveOrEdit');
+      switch(reminder.saveOrEdit){
+        case 'save':
+          reminder.date = new Date(reminder.date);
+          this.get('store').createRecord('reminder', reminder).save().then(() => {
+            this.setProperties({ title: '', date: '', notes: '' });
+          });
+          break;
+        case 'edit':
+          // this.transitionToRoute('reminders.reminder', reminder);
+          let reminder = this.getProperties('id','title', 'date', 'notes', 'saveOrEdit');
+          console.log(reminder);
+          this.get('router').transitionTo('reminders.reminder', reminder.id);
+        break;
+      }
     }
   }
 });
