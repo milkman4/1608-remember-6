c7f4600a5509e5b8afa5ee6fe772343671517173
MilkMan90 <mlkaufman747@gmail.com>
2016-12-11 19:49:17 -0700

    weird save is not a function bug

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -2,8 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  router: Ember.inject.service('-routing'),
-
+  
   title: '',
   date: '',
   notes: '',
@@ -11,18 +10,15 @@ export default Ember.Component.extend({
 
   actions: {
     handleSave() {
-      let reminder = this.getProperties('title', 'date', 'notes', 'saveOrEdit');
+      let reminder = this.getProperties('title', 'date', 'notes', 'save');
       if(reminder.save) {
           reminder.date = new Date(reminder.date);
           this.get('store').createRecord('reminder', reminder).save().then(() => {
             this.setProperties({ title: '', date: '', notes: '' });
           });
       } else {
-          let reminder = this.getProperties('id','title', 'date', 'notes', 'saveOrEdit');
-          console.log(reminder);
-          this.get('router').transitionTo('reminders.reminder', reminder.id);
+        console.log('asuh dude');
         }
       }
     }
-  }
 });
