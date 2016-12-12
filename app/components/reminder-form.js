0a29b242a602d3addc0d399da2ba36819fe05d3b
MilkMan90 <mlkaufman747@gmail.com>
2016-12-11 20:10:52 -0700

    weird save bug is fixed

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  
+
   title: '',
   date: '',
   notes: '',
@@ -10,14 +10,17 @@ export default Ember.Component.extend({
 
   actions: {
     handleSave() {
-      let reminder = this.getProperties('title', 'date', 'notes', 'save');
-      if(reminder.save) {
-          reminder.date = new Date(reminder.date);
+      let reminder = this.getProperties('title', 'date', 'notes', 'edit');
+      reminder.date = new Date(reminder.date);
+      if(!reminder.edit) {
           this.get('store').createRecord('reminder', reminder).save().then(() => {
             this.setProperties({ title: '', date: '', notes: '' });
           });
       } else {
-        console.log('asuh dude');
+          console.log('asuh dude');
+          this.get('store').findRecord('reminder', reminder.id).then((reminder) => {
+            reminder.save()
+          });
         }
       }
     }
